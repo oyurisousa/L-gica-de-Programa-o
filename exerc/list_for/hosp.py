6047e654b67859348837f8d8cc8d0a703656fb03
Carlos Yuri <82901729+oyurisousa@users.noreply.github.com>
2023-01-02 15:56:06 +0000

    hospedagem

---
+++ exerc/list_for/hosp.py
@@ -0,0 +1,29 @@
+nomes = []
+cpf = []
+quant_quartos = 0
+while True:
+    quarto = input(f"deseja um quarto?(s/n) ")
+    if quarto == "s":
+        quant_quartos += 1
+    elif quarto == "n":
+        print("nenhum quarto escolhido.")
+        break    
+
+    quant_pessoas = int(input("quantas pessoas serão hospedadas no quarto? "))
+
+    for x in range(quant_pessoas):
+        nomes.append(input(f"nome da pesoa {x+1}: "))
+        for i in range(1):
+            cpf.append(input(f"CPF de {nomes [x]}: "))
+            print("")
+
+    for x in range(1):
+        print("==============================================")
+        print(f"QUARTO {quant_quartos}")
+        print(f"numero de hóspedes: {quant_pessoas}")
+        print("______")
+
+        for x in range(quant_pessoas):
+            print(f"nome: {nomes[x]}")
+            print(f"CPF: {cpf[x]}")
+            print("--------------------")
